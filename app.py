946824ef12b63954114497acd0073c8a01c1496e
NPS-Cisco-2019 <54304816+NPS-Cisco-2019@users.noreply.github.com>
2019-11-25 11:20:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ def my_index():
     return flask.render_template("index.html", token="Hello world")
 
 app.run(debug=True)
+@app.route("/Firefox")
+@app.route("/Chrome")
+@app.route("/Safari")
+@app.route("/Picture")
+@app.route("/Answer")
+@app.route("/Settings")
+@app.route("/Saved Answers")
+@app.route("/GradeChoice")
+@app.route("/Tutorial")
